6a69a4dff95882f30a250e6316e442ac61cb265d
Noman <80441451+nali556@users.noreply.github.com>
2023-03-20 13:50:48 -0400

    login component created, centered login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 //Component Imports
 import Home from './components/Home'
+import Login from './components/Login'
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home></Home>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>)
    
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/login.css"
+
+export default function Login() {
+    return (
+        <>
+            <div className="login-container">
+                <div className="login">
+                    Hello
+                </div>
+            </div>
+        </>
+    )
+}
