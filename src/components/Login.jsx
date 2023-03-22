a4e6fc5420389f622a2401b6b66211507b67f4d3
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-03-21 21:24:24 -0400

    setting up form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,20 @@ export default function Login() {
     return (
         <>
             <div className="login-container">
-                <div className="login">
-                    Hello
+                <div className="login-wrap">
+                    <div className='login-title'>
+                        Log In
+                    </div>
+                    <div className='login-form'>
+                        <div>
+                            <label for="username">Username</label>
+                            <input className="form-username" id="username" placeholder='Username'></input>
+                        </div>
+                        <div>
+                            <label for="password">Password</label>
+                            <input className="form-password" id="password" placeholder='Password'></input>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
