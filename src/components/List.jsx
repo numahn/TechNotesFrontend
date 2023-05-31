c14622d2ef58269ff5e722893a4f8a691828eb28
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-05-30 22:56:31 -0400

    building skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../css/list.css"
+export default function List() {
+  return (
+    <>
+      <div className="list-container">
+        <div className="list-title">My List</div>
+        <div className="lists">
+          <div className="list">asdf</div> 
+        </div>
+      </div>
+    </>
+  )
+}
