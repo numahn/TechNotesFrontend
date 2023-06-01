e21896b041f3ae0d1b699fe0920e303ad14c12a2
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-05-31 23:06:09 -0400

    added sections

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,7 +8,10 @@ export default function List() {
       <Navbar/>
       <div className="list-container">
         <div className="list-title">My Notes</div>
-        <div className="sections"></div>
+        <div className="sections">
+          <p className="title">Title</p>
+          <p className="date">Date</p>
+        </div>
         <div className="lists">
           <div className="list">asdf</div> 
         </div>
